6fdb482aaed73dab717fed601bd9159480a2773f
sangyoo91 <sangyoo.91@gmail.com>
2015-05-14 22:00:24 +0900

    Added Basic Account Functionality
    Added Navbar dropdown

---
+++ client/global/register-helpers.js
@@ -0,0 +1,11 @@
+Template.registerHelper('avatar', function () {
+  var user = Meteor.user();
+  if( user && user.profile ){
+    //Show user profile
+    return user.profile.avatar;
+  } else {
+    // If user doesn't exist
+    // Show default avatar
+    return '/img/avatar.png';
+  }
+});
