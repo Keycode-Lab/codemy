3865919e5e31b30581c65f5c46c51eb674dfb70c
sangyoo91 <sangyoo.91@gmail.com>
2015-05-15 23:58:31 +0900

    Added Editor UI
    Fixed Layout

--- client/template/editor.js
+++ client/template/editor.js
@@ -6,11 +6,22 @@ Template.editor.events({
       Session.set('editor-content', '');
       Session.set('editor-content', content);
     },100);
+  },
+  'click .btn-preview': function (e) {
+    var $editorWrapper = $('.editor-wrapper');
+    if ( $editorWrapper.hasClass('show-preview') ) {
+      $editorWrapper.removeClass('show-preview');
+    } else {
+      $editorWrapper.addClass('show-preview');
+    }
   }
 });
 
 Template.editor.onRendered( function () {
-  //Autosize of Content Text Area (this is a plugin)
+  // Autosize of Content Text Area (this is a plugin)
   $('#editor-content').autosize();
-})
+
+  // Set Default Value of Preview to default
+  Session.set('editor-content', '**마크다운 미리보기**');
+});
 
