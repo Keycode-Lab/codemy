59817317332533048fa3fc3b96d4aa3bbf47676b
sangyoo91 <sangyoo.91@gmail.com>
2015-06-20 09:30:11 +0900

    Add Post Item Vote Helper
    - Shows differently for negative, zero, positive vote counts

--- client/template/post/post_item.js
+++ client/template/post/post_item.js
@@ -1,4 +1,13 @@
 Template.postItem.helpers({
+  negativeVote: function () {
+    return this.votes < 0;
+  },
+  zeroVote: function () {
+    return this.votes === 0;
+  },
+  positiveVote: function () {
+    return this.votes > 0;
+  },
   answered: function () {
     return this.answered === true;
   }
